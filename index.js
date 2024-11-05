5dd1a0efc443699893db16c18f3d9ac9c2e900d1
Sarahalhbaish <sarah21995@live.com>
2024-11-05 19:07:41 +0300

    Task Complete

---
+++ index.js
@@ -0,0 +1,37 @@
+const cities = [
+    {
+      "id": 301,
+      "name": "New York",
+      "population": 8398748,
+      "country": "United States"
+    },
+    {
+      "id": 302,
+      "name": "Paris",
+      "population": 2140526,
+      "country": "France"
+    },
+    {
+      "id": 303,
+      "name": "Tokyo",
+      "population": 13960000,
+      "country": "Japan"
+    },
+    {
+      "id": 304,
+      "name": "Sydney",
+      "population": 5230330,
+      "country": "Australia"
+    },
+    {
+      "id": 305,
+      "name": "Cairo",
+      "population": 9121515,
+      "country": "Egypt"
+    }
+  ];
+
+  
+/* *********************************************************************************** */
+
+
