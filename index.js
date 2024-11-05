7006900fc297a0989ef37457e2fdae5d0484a3eb
Sarahalhbaish <sarah21995@live.com>
2024-11-05 19:24:37 +0300

    Task Complete

--- index.js
+++ index.js
@@ -35,3 +35,42 @@ const cities = [
 /* *********************************************************************************** */
 
 
+/* ----------------- Q.1 ----------------- */
+function getCityPopulation(city) {
+    return city.population;
+}
+//   console.log(getCityPopulation(cities[0]));
+
+
+/* ----------------- Q.2 ----------------- */
+function isPopulationAboveThreshold(city, threshold) {
+    if (city.population >= threshold)
+        return  true;
+    else return false;
+}
+//   console.log(isPopulationAboveThreshold(cities[1], 2000000));
+
+  
+/* ----------------- Q.3 ----------------- */
+function addCity(cities, city) {
+    cities.push(city);
+    return cities;
+  }
+  
+  const newCity = {
+    "id": 306,
+    "population": 1200000,
+    "country": "Canada"
+  };
+  
+//   console.log(addCity(cities, newCity));
+
+  
+/* ----------------- Q.4 ----------------- */
+function countCitiesInCountry(cities, country) {
+    let count = 0;
+    if (cities.country = country)
+        count++;
+    return count;
+  }
+//   console.log(countCitiesInCountry(cities, "France"));
